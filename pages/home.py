1115f79651d65e6429c3a2dffb8ceaf7fc9bb950
ojasvijain <41644996+ojasvijain@users.noreply.github.com>
2024-04-18 10:39:57 +1000

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -25,6 +25,29 @@ def b64_image(img):
 
 years = ['2019', '2020', '2021', '2022', '2023']
 new_species_counts = [39, 19, 44, 37, 144]
+graph_component = dcc.Graph(
+    id='species-line-chart',
+    figure={
+        'data': [
+            go.Scatter(
+                x=years,
+                y=new_species_counts,
+                mode='lines+markers',
+                marker=dict(color='#F9F1E8'),
+                line=dict(color='#F9F1E8')
+            ),
+        ],
+        'layout': go.Layout(
+            plot_bgcolor='#545646',
+            paper_bgcolor='#545646',
+            font=dict(color='#F9F1E8'),
+            xaxis=dict(title='Year', color='#F9F1E8'),
+            yaxis=dict(title='New Endangered Species Count', color='#F9F1E8'),
+            title=dict(text='Endangered Species Counts Over Years', font=dict(color='#F9F1E8')),
+            margin=dict(l=100),
+        )
+    }
+)
 
 clientside_callback(
     ClientsideFunction(namespace='clientside', function_name='trigger_gsap_animation'),
@@ -57,109 +80,79 @@ layout = html.Div([
     dcc.Location(id='url', refresh=False),
 
     html.Section(className='parallax', children=[
-
-        # html.Img(src='/assets/monutain_01.png', id='m1'),
-        # html.Img(src='/assets/trees_02.png', id='t2'),
-        # html.Img(src='/assets/monutain_02.png', id='m2'),
-        # html.Img(src='/assets/trees_01.png', id='t1'),
-        # html.Img(src='/assets/man.png', id='man'),
-        # html.Img(src='/assets/plants.png', id='plants'),
         dbc.Row([
             dbc.Col([
+                # html.Img(src='/assets/element1.png', style={'margin-right':'2500px', 'height':'100%', 'width':'70%', 'margin-left':'0px'}),
                 html.H1('Discover.', style={'font-size': '3em'}),
                 html.P(""),
-                html.H1('Encounter.', style={'font-size': '3em'}),
+                html.H1('Educate.', style={'font-size': '3em'}),
                 html.P(""),
                 html.H1('Conserve.', style={'font-size': '3em'}),
                 html.P(""),
                 html.P("Join us in exploring and", style={'font-size': '2em'}),
                 html.P("protecting Victoria's biodiversity", style={'font-size': '2em'}),
                 html.Button("Learn More", id="learn-more-btn", n_clicks=0, 
-                            style={'text-decoration': 'none', 'padding': '6px 15px', 'background': '#545646', 'color': '#F9F1E8', 'border-radius': '20px'}),
-            ], width=6),  # This sets the column to take half of the row (6 out of 12 columns)
+                            style={'text-decoration': 'none', 'padding': '6px 15px', 'background': '#545646', 'color': '#F9F1E8', 'border-radius': '20px', 'margin-top':'30px'}),
+            ], id='t1', width=6, lg=7, style={'margin-right':'900px'}),  # This sets the column to take half of the row (6 out of 12 columns)
             dbc.Col([
-                html.Img(src='/assets/home.png', style={'height': '50%', 'width': '30%'})
-            ], width=6)  # This sets the other column also to take half of the row
-        ], justify="start", style={'display': 'flex'})
+                html.Img(src='/assets/element2.png', id='t2', style={'height': '85%', 'width': '55%', 'margin-left':'1150px', 'margin-bottom':'170px'}),
+                html.Img(src='/assets/home.png', id='t2', style={'height': '60%', 'width': '37%', 'margin-left':'1000px', 'margin-right':'30%', 'margin-bottom':'120px', 'border': '10px solid #545646', 'border-radius': '20px'})
+            ], width=6, lg=5)  # This sets the other column also to take half of the row
+        ], style={'display': 'flex'})
         # html.Button()
     ]),
-    html.Div(id="additional-content", style={'display': 'none'}, children=[
-        html.P("Here's some additional information you might find interesting..."),
-        html.Button("^", id="retract-btn", style={
-                'position': 'relative', 'top': '5px', 'right': '10px', 'cursor': 'pointer'
-        })
+    html.Div([
+        html.Div([
+            html.Div([
+                html.P("The number of endangered species in Victoria rose significantly in the past year.", style={'padding': '30px', 'color': '#F9F1E8', 'font-size': '1.5em', 'margin-top': '40px'}),
+                html.P(""),
+                html.P("> Participate in conservation and seasonal activities around you to safeguard endangered species in Victoria.", style={'color': '#F9F1E8', 'font-size':'1em', 'margin-left':'40px'}),
+                html.P(""),
+                html.P("> Navigate through the diverse flora and fauna around you through the myriad of hiking options.", style={'color': '#F9F1E8', 'font-size':'1em', 'margin-left':'40px'}),
+                html.P(""),
+                html.P("> Get updated with species found on your hike real-time.", style={'color': '#F9F1E8', 'font-size':'1em', 'margin-left':'40px'}),
+                html.P(""),
+                html.P("> Explore the Victorian biodiversity with us.", style={'color': '#F9F1E8', 'font-size':'1em', 'margin-left':'40px'}),
+                # html.P(""),
+                # Position the retract button absolutely within the relatively positioned parent div
+                html.Button("^", id="retract-btn", style={
+                    'position': 'absolute',
+                    'top': '10px',
+                    'right': '10px',
+                    'padding': '10px 20px',
+                    'font-size': '16px',
+                    'cursor': 'pointer',
+                    'background-color': '#545646',
+                    'color': '#F9F1E8',
+                    'border': 'none',
+                    'border-radius': '5px'
+                })
+            ], style={'position': 'relative', 'flex': '1', 'padding': '20px'}),  # Parent div must be positioned relatively
+            html.Div([graph_component], style={'flex': '1', 'padding': '20px'})
+        ], style={'display': 'flex'})
+    ], id="additional-content", style={'display': 'none'}),
+    html.Div([
+        html.Div([
+            html.A(href="/my-trails", children=[
+                html.Img(src=b64_image('assets/circle1.png'), style={'width': '100%', 'height': 'auto'}),
+                html.P("Over 30 venomous species", className="default-text", style={'top': '50%', 'left': '55%'}),
+                html.P("Get real-time updates on your hike", className="hover-text", style={'top': '50%', 'left': '55%'})
+            ], className="circle", id='right2'),
+            html.A(href="/all-trails", children=[
+                html.Img(src=b64_image('assets/circle2.png'), style={'width': '100%', 'height': 'auto'}),
+                html.P("140 new endangered species", className="default-text", style={'top': '50%', 'left': '50%'}),
+                html.P("Get trails based on species you want to see", className="hover-text", style={'top': '50%', 'left': '50%'})
+            ], className="circle"),
+            html.A(href="/page3", children=[
+                html.Img(src=b64_image('assets/circle3.png'), style={'width': '100%', 'height': 'auto'}),
+                html.P("2700 conservation parks", className="default-text", style={'top': '55%', 'left': '55%'}),
+                html.P("Discover conservation events around you", className="hover-text", style={'top': '55%', 'left': '55%'})
+            ], className="circle", id='left1')
+        ], style={'display': 'flex', 'justify-content': 'space-around', 'align-items': 'center', 'padding': '50px'})
     ]),
-
+    
     html.Div(id='dummy-input', style={'display': 'none'}),
-    html.Div(id='dummy-output', style={'display': 'none'}),
-    # For the divs with id="right", place the icon after the text
-    
-    
-    
-    
-    html.Div([
-        html.H6([
-            "Every trail you explore can be a step towards conservation. Learn about the unique flora and fauna that make Victoria a biodiversity hotspot.",
-            html.I(className="fas fa-seedling", style={'margin-left': '10px', 'color': '#fff'})
-        ], style={'margin-top': '20px', 'margin-bottom': '10px', 'color': '#fff'})
-    ], id="right1", style={'background-color': '#112434', 'border': '2px solid white', 'padding': '40px', 'border-radius': '50px', 'margin-right': '150px', 'margin-top': '40px'}),
-
-    # For the divs with id="left", place the icon before the text
-    html.Div([
-        html.H6([
-            html.I(className="fas fa-exclamation-triangle", style={'margin-right': '10px', 'color': '#fff'}),
-            "In just one year, the number of endangered species in regional Victoria has surged from 57 to 144 due to environmental changes and human activities."
-        ], style={'margin-top': '20px', 'margin-bottom': '10px', 'color': '#fff', 'text-align': 'right'})
-    ], id="left1", style={'background-color': '#112434', 'border': '2px solid white', 'padding': '40px', 'border-radius': '50px', 'margin-left': '150px', 'margin-top': '40px'}),
-
-    # Use our interactive maps and filters to find trails...
-    html.Div([
-        html.H6([
-            "Use our interactive maps and filters to find trails where you can encounter Victoria's endangered and native species in their natural habitat.",
-            html.I(className="fas fa-map-marked-alt", style={'margin-left': '10px', 'color': '#fff'})
-        ], style={'margin-top': '20px', 'margin-bottom': '10px', 'color': '#fff'})
-    ], id="right2", style={'background-color': '#112434', 'border': '2px solid white', 'padding': '40px', 'border-radius': '50px', 'margin-right': '150px', 'margin-top': '40px'}),
-
-    # Share your findings...
-    html.Div([
-        html.H6([
-            html.I(className="fas fa-share-alt", style={'margin-right': '10px', 'color': '#fff'}),
-            "Share your findings and get the latest updates on conservation efforts, upcoming events, and new trail discoveries. Together, we can make a difference!"
-        ], style={'margin-top': '20px', 'margin-bottom': '10px', 'color': '#fff', 'text-align': 'right'})
-    ], id="left2", style={'background-color': '#112434', 'border': '2px solid white', 'padding': '40px', 'border-radius': '50px', 'margin-left': '150px', 'margin-top': '40px'}),
-    html.Section(children=[
-        dbc.Row([
-            dbc.Col(
-                html.Div(children=[
-                    # html.H1(children='New Endangered Species Counts Over Years'),
-                    dcc.Graph(
-                        id='species-line-chart',
-                        figure={
-                            'data': [
-                                go.Scatter(
-                                    x=years,
-                                    y=new_species_counts,
-                                    mode='lines+markers',
-                                    marker=dict(color='#fff'),
-                                    line=dict(color='#fff')
-                                ),
-                            ],
-                            'layout': go.Layout(
-                                plot_bgcolor='#112434',
-                                paper_bgcolor='#112434',
-                                font=dict(color='#fff'),
-                                xaxis=dict(title='Year', color='#fff'),
-                                yaxis=dict(title='New Endangered Species Count', color='#fff'),
-                                title=dict(text='Endangered Species Counts Over Years', font=dict(color='#fff')),
-                                margin=dict(l=100),
-                            )
-                        }
-                    )
-                ], style={'borderRadius': '50px', 'overflow': 'hidden'}), id='line_c'),
-            dbc.Col(html.Div(children=[dcc.Graph(figure=pie_chart)],
-                    style={'borderRadius': '50px', 'overflow': 'hidden'}), id='pie')
-        ])
-    ], style={'margin-top': '40px', 'padding': '40px'})
+    html.Div(id='dummy-output', style={'display': 'none'})
 ])
 
 @callback(
@@ -170,7 +163,14 @@ layout = html.Div([
 def toggle_additional_content(learn_more_clicks, retract_clicks, current_style):
     triggered_id = dash.callback_context.triggered[0]['prop_id'].split('.')[0]
     if triggered_id == 'learn-more-btn' and learn_more_clicks:
-        return {'display': 'block'} if current_style['display'] == 'none' else {'display': 'none'}
+        return {
+                'background-color': '#545646',
+                'border': '2px solid white',
+                'padding': '10px',
+                'border-radius': '15px',
+                'margin-left': '20px',
+                'margin-right': '20px'
+                } if current_style['display'] == 'none' else {'display': 'none'}
     elif triggered_id == 'retract-btn':
         return {'display': 'none'}
     return current_style
